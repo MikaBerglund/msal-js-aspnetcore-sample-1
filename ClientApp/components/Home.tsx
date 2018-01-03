0d0a0e93c840cca92cc765275287798669938e78
Mika Berglund <mika.berglund@chosense.com>
2018-01-04 01:40:42 +0200

    Added required code to get MSAL authentication to work.

--- ClientApp/components/Home.tsx
+++ ClientApp/components/Home.tsx
@@ -1,14 +1,27 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { UserAgentApplication } from "msal";
+import { User } from "node_modules/msal/lib-commonjs/User";
 
 export interface IUserInfoState {
-
+    currentUser?: User;
 }
 
 export class Home extends React.Component<RouteComponentProps<{}>, IUserInfoState> {
-    public render() {
-        var content = this.renderAnonymous();
+    constructor() {
+        super();
 
+        this.state = {};
+
+        this.msalApp = new UserAgentApplication("08275f8c-4635-40d0-a038-26f6623d511e", "https://login.microsoftonline.com/common", undefined, {
+            redirectUri: "http://localhost:5000"
+        });
+    }
+
+    private msalApp: UserAgentApplication;
+
+    public render() {
+        var content = this.state.currentUser ? this.renderUserInfo() : this.renderAnonymous();
         return content;
     }
 
@@ -23,17 +36,27 @@ export class Home extends React.Component<RouteComponentProps<{}>, IUserInfoStat
 
     private renderUserInfo(): JSX.Element {
         return <div>
-            
+            <h1>{ this.state.currentUser.name }</h1>
+            <ul className="list-group">
+                <li className="list-group-item">{ this.state.currentUser.displayableId }</li>
+                <li className="list-group-item">{ this.state.currentUser.identityProvider }</li>
+                <li className="list-group-item">{ this.state.currentUser.userIdentifier }</li>
+            </ul>
             <button className="btn btn-info" onClick={ this.logout.bind(this) }>Log out</button>
         </div>;
     }
 
     private login(): Promise<any> {
-
-        return Promise.resolve();
+        return this.msalApp
+            .loginPopup(["user.read"])
+            .then(idToken => {
+                var usr = this.msalApp.getUser();
+                console.log("Current user", usr);
+                this.setState({ currentUser: usr });
+            });
     }
 
     private logout(): void {
-
+        this.msalApp.logout();
     }
 }
